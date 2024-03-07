6178b6e2bd4d9d73a5fe9b8ad3eab0ac24d7d883
ravi <rduttdogra@gmail.com>
2024-03-07 15:45:49 +0530

    handle async function utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+//async handler with promise 
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((error) => next(error))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+//async handler with try catch and async await
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        res.status(error.code || 500).json({
+            sucess: false,
+            message: error.message
+        })
+    }
+}
+*/
