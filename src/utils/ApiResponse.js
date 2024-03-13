9f0a00ac12bd85842a65931bf9a8fdaf44a6353b
ravi <rduttdogra@gmail.com>
2024-03-13 14:13:45 +0530

    cloudinary and multer config and intigrate for fileupload

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,7 +3,7 @@ class ApiResponse {
         this.statusCode = statusCode
         this.data = data
         this.message = message
-        this.sucess = statusCode
+        this.sucess = statusCode < 400
     }
 }
 
